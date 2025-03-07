32ac6461c6ad28865cdad1a1e634faab3ec5a5a7
Akropolidis <adeyanju_charles@yahoo.ca>
2025-03-07 14:17:59 -0500

    Modified main.c to integrate bluetooth code

--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,327 @@
-/* Added averaging */
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "app_bluenrg_ms.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "AS7421.h"
+#include <inttypes.h>
+#include <limits.h>
+#include "mux.h"
+#include "gpio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define NUM_CHANNELS		64
+#define AVERAGE_COUNT       30
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	MX_BlueNRG_MS_Init();
+	/* USER CODE BEGIN 2 */
+	GPIO_Init();
+	I2C1_Init();
+
+	/* Mux *
+	 * Note: calling enableChannel closes all the mux outputs before opening the specified channel
+	 * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
+	 * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
+	enableChannel(CHANNEL_1);
+
+	/* Sensor */
+	// Containers to receive channel data
+	uint16_t channel_data[CHANNELSIZE];
+	uint16_t temp_data[CHANNELSIZE];
+
+	uint32_t sum_data[CHANNELSIZE] = {0};
+	float avg_data[CHANNELSIZE] = {0};
+
+	startup();
+	HAL_Delay(2000);
+	startMeasurements(true);
+
+	int count = 0;
+
+	/* Infinite loop */
+	while (measurementActive()) {
+
+		char uart_buf[1000];
+		int uart_buf_len;
+		performMeasurements(channel_data, temp_data);
+		printf("%i\n\r", -1);
+		printf("%i\n\r", count);
+
+		// Accumulate the readings
+		for (int i = 0; i < CHANNELSIZE; i++){
+		   sum_data[i] += channel_data[i];
+		}
+
+		if (count >= AVERAGE_COUNT){
+			for (int i = 0; i < CHANNELSIZE; i++){
+				avg_data[i] = (float)sum_data[i] / AVERAGE_COUNT;
+				sum_data[i] = 0; //Reset sum for next averaging window
+			}
+
+			for (int i = 0; i < CHANNELSIZE; i++){
+				uart_buf_len = sprintf(uart_buf, "%.3f\n\r", avg_data[i]);
+				HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+
+			}
+			MX_BlueNRG_MS_Process(avg_data);
+			count = 0;
+		}
+		count++;
+
+		/* Blue pushbutton breaks out of the while loop to put the device to sleep,
+		 * have to reset the board to restart measurements */
+		if (UserBtn_Control())
+		{
+			break;
+		}
+	}
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 64;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_8, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA1 PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+
+///* Testing MAIN */
 //#include "stm32f4xx.h"
 //#include "AS7421.h"
 //#include <inttypes.h>
@@ -6,125 +329,127 @@
 //#include "mux.h"
 //
 //
-//#define AVERAGE_COUNT       30
+//#define NUM_CHANNELS        64
 //
 //int main(void)
 //{
-//	I2C1_Init();
+//    I2C1_Init();
 //
-//	/* Mux *
-//	 * Note: calling enableChannel closes all the mux outputs before opening the specified channel
-//	 * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
-//	 * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
-//	enableChannel(CHANNEL_1);
+//    /* Mux *
+//     * Note: calling enableChannel closes all the mux outputs before opening the specified channel
+//     * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
+//     * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
+//    enableChannel(CHANNEL_1);
 //
-//	/* Sensor */
-//	// Containers to receive channel data
-//	uint16_t channel_data[CHANNELSIZE];
-//	uint16_t temp_data[CHANNELSIZE];
+//    /* Sensor */
+//    // Containers to receive channel data
+//    uint16_t channel_data[CHANNELSIZE];
+//    uint16_t temp_data[CHANNELSIZE];
 //
-//	uint32_t sum_data[CHANNELSIZE] = {0};
-//	float avg_data[CHANNELSIZE] = {0};
+//    startup();
+//    delayMillis(2000);
+//    startMeasurements(true);
 //
-//	startup();
-//	delayMillis(2000);
-//	startMeasurements(true);
-//
-//	int count = 0;
-//	while (measurementActive())
-//	{
-//		performMeasurements(channel_data, temp_data);
-//		printf("%i\n\r", -1);
-//		printf("%i\n\r", count);
-//
-//        // Accumulate the readings
-//        for (int i = 0; i < CHANNELSIZE; i++)
+////    unsigned long start = getMillis();
+////    unsigned long duration = 120000; // aka 2mins
+//    int count = 0;
+//    while (measurementActive())
+//    {
+//        performMeasurements(channel_data, temp_data);
+//        printf("%i\n\r", -1);
+//        printf("%i\n\r", count);
+//        for (int i = 0; i < NUM_CHANNELS; i++)
 //        {
-//            sum_data[i] += channel_data[i];
+////            printf("Channel %i: %f\n\r", i+1, channel_data[i]);
+//            printf("%d\n\r", channel_data[i]);
 //        }
-//
-//
-//        if (count >= AVERAGE_COUNT)
-//		{
-//			for (int i = 0; i < CHANNELSIZE; i++)
-//			{
-//				avg_data[i] = (float)sum_data[i] / AVERAGE_COUNT;
-//				sum_data[i] = 0; //Reset sum for next averaging window
-//			}
-//
-//            for (int i = 0; i < CHANNELSIZE; i++)
-//            {
-//            	printf("%.3f\n\r", avg_data[i]);
-//            }
-//
-//            count = 0;
-//		}
-//
 //        count++;
+//        if(count ==    INT_MAX){
+//            count = 0;
+//        }
+//        //unsigned long end = getMillis();
+//        // If in continuous mode, specify duration of test
+//        //if (end - start > duration)
+//        //{
+//        //    break;
+//        //}
+//    }
+//    stopMeasurements();
+//    sleep();
 //
-//	}
-//	stopMeasurements();
-//	sleep();
-//
-//	return 0;
+//    return 0;
 //}
 
 
-/* OLD MAIN */
-#include "stm32f4xx.h"
-#include "AS7421.h"
-#include <inttypes.h>
-#include <limits.h>
-#include "mux.h"
 
 
-#define NUM_CHANNELS        64
 
-int main(void)
-{
-    I2C1_Init();
 
-    /* Mux *
-     * Note: calling enableChannel closes all the mux outputs before opening the specified channel
-     * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
-     * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
-    enableChannel(CHANNEL_1);
 
-    /* Sensor */
-    // Containers to receive channel data
-    uint16_t channel_data[CHANNELSIZE];
-    uint16_t temp_data[CHANNELSIZE];
 
-    startup();
-    delayMillis(2000);
-    startMeasurements(true);
 
-//    unsigned long start = getMillis();
-//    unsigned long duration = 120000; // aka 2mins
-    int count = 0;
-    while (measurementActive())
-    {
-        performMeasurements(channel_data, temp_data);
-        printf("%i\n\r", -1);
-        printf("%i\n\r", count);
-        for (int i = 0; i < NUM_CHANNELS; i++)
-        {
-//            printf("Channel %i: %f\n\r", i+1, channel_data[i]);
-            printf("%d\n\r", channel_data[i]);
-        }
-        count++;
-        if(count ==    INT_MAX){
-            count = 0;
-        }
-        //unsigned long end = getMillis();
-        // If in continuous mode, specify duration of test
-        //if (end - start > duration)
-        //{
-        //    break;
-        //}
-    }
-    stopMeasurements();
-    sleep();
 
-    return 0;
-}
+
+
+
+
+
+
+///* Testing MAIN plus Active-Idle button*/
+//#include "stm32f4xx.h"
+//#include "AS7421.h"
+//#include <inttypes.h>
+//#include <limits.h>
+//#include "mux.h"
+//#include "gpio.h"
+//
+//
+//#define NUM_CHANNELS        64
+//
+//int main(void)
+//{
+//	GPIO_Init();
+//    I2C1_Init();
+//
+//    /* Mux *
+//     * Note: calling enableChannel closes all the mux outputs before opening the specified channel
+//     * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
+//     * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
+//    enableChannel(CHANNEL_1);
+//
+//    /* Sensor */
+//    // Containers to receive channel data
+//    uint16_t channel_data[CHANNELSIZE];
+//    uint16_t temp_data[CHANNELSIZE];
+//
+//    startup();
+//    delayMillis(2000);
+//    startMeasurements(true);
+//
+//    int count = 0;
+//    while (measurementActive())
+//    {
+//        performMeasurements(channel_data, temp_data);
+//        printf("%i\n\r", -1);
+//        printf("%i\n\r", count);
+//        for (int i = 0; i < NUM_CHANNELS; i++)
+//        {
+//            printf("%d\n\r", channel_data[i]);
+//        }
+//        count++;
+//        if(count == INT_MAX){
+//            count = 0;
+//        }
+//
+//        /* Blue pushbutton breaks out of the while loop to put the device to sleep,
+//         * have to reset the board to restart measurements */
+//        if (UserBtn_Control())
+//        {
+//            break;
+//        }
+//    }
+//    stopMeasurements();
+//    sleep();
+//
+//    return 0;
+//}
