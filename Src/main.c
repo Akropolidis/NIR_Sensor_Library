cc0c98729b3b29d3e44a4c41b50f3d5bb50468d8
Akropolidis <adeyanju_charles@yahoo.ca>
2025-03-09 22:42:11 -0400

    Added button code to isolate bluetooth and sensor operation

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,14 @@ int main(void)
      * Note: calling enableChannel closes all the mux outputs before opening the specified channel
      * Switching between channels puts previously ON channel in idle mode (LED still on but not measuring)
      * Sensor state of previously ON channel resets to sleep mode with power on reset (i.e. power off then power on)*/
-    enableChannel(CHANNEL_1);
+    enableChannel(CHANNEL_0);
 
     /* Sensor */
     // Containers to receive channel data
     uint16_t channel_data[CHANNELSIZE];
     uint16_t temp_data[CHANNELSIZE];
 
+	while(!UserBtn_Control()){} //stay here until button is pressed
     startup();
     delayMillis(2000);
     startMeasurements(true);
